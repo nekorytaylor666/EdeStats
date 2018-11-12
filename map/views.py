e753f7f83906a36c0694452208a4800375fa3e36
nekorytaylor666 <art_gulba@mail.ru>
2018-11-12 20:52:36 +0600

    static folder

--- map/views.py
+++ map/views.py
@@ -4,5 +4,5 @@ def index(request):
     context = {
         "items" : 0
     }
-    return render(request,"style.html",context)
+    return render(request,"base.html",context)
 # Create your views here.
