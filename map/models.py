7730d98bf03ab7063bcc75b77c67ec1d1ef44cd9
nekorytaylor666 <art_gulba@mail.ru>
2018-11-13 23:46:24 +0600

    removed the init script

--- map/models.py
+++ map/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class School(models.Model):
     name = models.CharField(max_length=200)
-    address = models.CharField(max_length = 200)
+    address = models.CharField(max_length=200)
     lat = models.FloatField(default=0)
     lng = models.FloatField(default=0)
     ent = models.FloatField(default=0)
--- map/views.py
+++ map/views.py
@@ -14,22 +14,22 @@ def index(request):
 
 
 def init_map(request):
-    schools = get_schools()
-    for school in schools:
-        school_dict = get_valid_schools(school)
-        if(school_dict is not None):
-            _name = school_dict["name"]
-            _address = school_dict["address"]
-            _lat = school_dict["lat"]
-            _lng = school_dict["lng"]
-            _ent = school_dict["ent"]
-            print(_name)
-            print(_address)
-            print(_lat)
-            print(_lng)
-            print(_ent)
-            obj_to_save = School(name = _name, address = _address, lat = _lat, lng = _lng, ent = _ent) 
-            obj_to_save.save()
+    # schools = get_schools()
+    # for school in schools:
+    #     school_dict = get_valid_schools(school)
+    #     if(school_dict is not None):
+    #         _name = school_dict["name"]
+    #         _address = school_dict["address"]
+    #         _lat = school_dict["lat"]
+    #         _lng = school_dict["lng"]
+    #         _ent = school_dict["ent"]
+    #         print(_name)
+    #         print(_address)
+    #         print(_lat)
+    #         print(_lng)
+    #         print(_ent)
+    #         obj_to_save = School(name = _name, address = _address, lat = _lat, lng = _lng, ent = _ent) 
+    #         obj_to_save.save()
     schools = School.objects.all()
     
     context = {
